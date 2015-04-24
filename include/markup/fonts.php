28f9b9ce8d6d4e654a4367aa4a274b4687553ec3
Henry Proudlove <henry.proudlove@gmail.com>
2015-04-24 10:55:12 +0800

    self hosted fonts tweak now working on staging cross browser

--- include/markup/fonts.php
+++ include/markup/fonts.php
@@ -1,26 +1,22 @@
-		<style type="text/css">
-		@font-face{
-		font-family:"AvenirNext-UltraLi";
-		src:url("<?php echo $page->rel_depth();?>fonts/17c55b27-e9ab-43cd-b948-d49f97480d68.eot?#iefix");
-		src:url("<?php echo $page->rel_depth();?>fonts/17c55b27-e9ab-43cd-b948-d49f97480d68.eot?#iefix") format("eot"),url("<?php echo $page->rel_depth();?>fonts/2e3f5cb9-101f-46cf-a7b3-dfaa58261e03.woff2") format("woff2"),url("<?php echo $page->rel_depth();?>fonts/fa19948e-5e38-4909-b31e-41acd170d6f2.woff") format("woff"),url("<?php echo $page->rel_depth();?>fonts/6de0ce4d-9278-467b-b96f-c1f5f0a4c375.ttf") format("truetype"),url("<?php echo $page->rel_depth();?>fonts/9fd4ea0c-b19a-4b21-9fdf-37045707dd78.svg#9fd4ea0c-b19a-4b21-9fdf-37045707dd78") format("svg");
-	    font-weight: normal;
-	    font-style: normal;
-		}
-		@font-face{
-		font-family:"AvenirNext-Medium";
-		src:url("<?php echo $page->rel_depth();?>fonts/1a7c9181-cd24-4943-a9d9-d033189524e0.eot?#iefix");
-		src:url("<?php echo $page->rel_depth();?>fonts/1a7c9181-cd24-4943-a9d9-d033189524e0.eot?#iefix") format("eot"),url("<?php echo $page->rel_depth();?>fonts/627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2") format("woff2"),url("<?php echo $page->rel_depth();?>fonts/f26faddb-86cc-4477-a253-1e1287684336.woff") format("woff"),url("<?php echo $page->rel_depth();?>fonts/63a74598-733c-4d0c-bd91-b01bffcd6e69.ttf") format("truetype"),url("<?php echo $page->rel_depth();?>fonts/a89d6ad1-a04f-4a8f-b140-e55478dbea80.svg#a89d6ad1-a04f-4a8f-b140-e55478dbea80") format("svg");
-	    font-weight: bold;
-	    font-style: normal;
-		}
-		</style>
-		<script type="text/javascript">
-		var MTIProjectId='6debb9f6-678b-474d-b007-9fa2baf9b402';
-		 (function() {
-		        var mtiTracking = document.createElement('script');
-		        mtiTracking.type='text/javascript';
-		        mtiTracking.async='true';
-		        mtiTracking.src=('https:'==document.location.protocol?'https:':'http:')+'//fast.fonts.net/t/trackingCode.js';
-		        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
-		   })();
-		</script> 
+<style type="text/css">
+@font-face{
+font-family:"AvenirNextLTW01-UltraLi";
+src:url("<?php echo $page->rel_depth();?>fonts/17c55b27-e9ab-43cd-b948-d49f97480d68.eot?#iefix");
+src:url("<?php echo $page->rel_depth();?>fonts/17c55b27-e9ab-43cd-b948-d49f97480d68.eot?#iefix") format("eot"),url("<?php echo $page->rel_depth();?>fonts/2e3f5cb9-101f-46cf-a7b3-dfaa58261e03.woff2") format("woff2"),url("<?php echo $page->rel_depth();?>fonts/fa19948e-5e38-4909-b31e-41acd170d6f2.woff") format("woff"),url("<?php echo $page->rel_depth();?>fonts/6de0ce4d-9278-467b-b96f-c1f5f0a4c375.ttf") format("truetype"),url("<?php echo $page->rel_depth();?>fonts/9fd4ea0c-b19a-4b21-9fdf-37045707dd78.svg#9fd4ea0c-b19a-4b21-9fdf-37045707dd78") format("svg");
+}
+@font-face{
+font-family:"AvenirNextLTW01-Medium";
+src:url("<?php echo $page->rel_depth();?>fonts/1a7c9181-cd24-4943-a9d9-d033189524e0.eot?#iefix");
+src:url("<?php echo $page->rel_depth();?>fonts/1a7c9181-cd24-4943-a9d9-d033189524e0.eot?#iefix") format("eot"),url("<?php echo $page->rel_depth();?>fonts/627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2") format("woff2"),url("<?php echo $page->rel_depth();?>fonts/f26faddb-86cc-4477-a253-1e1287684336.woff") format("woff"),url("<?php echo $page->rel_depth();?>fonts/63a74598-733c-4d0c-bd91-b01bffcd6e69.ttf") format("truetype"),url("<?php echo $page->rel_depth();?>fonts/a89d6ad1-a04f-4a8f-b140-e55478dbea80.svg#a89d6ad1-a04f-4a8f-b140-e55478dbea80") format("svg");
+}
+</style>
+<script type="text/javascript">
+var MTIProjectId='6debb9f6-678b-474d-b007-9fa2baf9b402';
+ (function() {
+        var mtiTracking = document.createElement('script');
+        mtiTracking.type='text/javascript';
+        mtiTracking.async='true';
+        mtiTracking.src=('https:'==document.location.protocol?'https:':'http:')+'//fast.fonts.net/t/trackingCode.js';
+        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
+   })();
+</script>
