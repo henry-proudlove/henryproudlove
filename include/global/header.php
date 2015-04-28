3971eb9ee49c4d2eaf12be7bd99ed9bd9a12fffb
Henry Proudlove <henry.proudlove@gmail.com>
2015-04-28 14:04:35 +0800

    Revert fca1583..65eeff4
    This rolls back to commit fca1583e6b5ee52cace6b56783be4510d555c197.

--- include/global/header.php
+++ include/global/header.php
@@ -48,18 +48,6 @@
 			$body_data = false;
 		}
 	?>
-	<style type="text/css">
-		.cssfilters.no-touchevents .cycle-active section.cycle,
-		.cssfilters.no-touchevents .idle .content{
-			-webkit-filter: url(#grad);
-					filter: url(#grad);
-		}
-		.cssfilters.no-touchevents .cycle-active.cycle-scroll section.cycle,
-		.cssfilters .content{
-			-webkit-filter: none;
-					filter: none;
-		}
-	</style>
     <body class="<?php echo $body_class . '"';  
 		if($body_data){
 			echo 'data-uri-stem="' . $body_data . '"';
