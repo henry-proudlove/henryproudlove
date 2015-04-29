73869bd8151191298de8b509af3ffb8208d06d90
Henry Proudlove <henry.proudlove@gmail.com>
2015-04-29 16:22:42 +0800

    description tweaks

--- include/markup/desc.php
+++ include/markup/desc.php
@@ -1,10 +1,10 @@
 <?php
 	$stem = 'Independent Creative // ';
+	$desc_default = $stem . 'Potfolio site showcasing some of my recent work';
 	$desc_arr = array(
 		'about'   => $stem . 'A decade of experience in the design and advertising industry producing great work for great brands',
 		'contact' => 'Get in touch with me // I might get in touch with you back',
 		'home'    => $desc_default,  
-		'work'    => 'Recent projectss'
+		'work'    => 'Recent projects'
 	);
-	$desc_default = $stem . 'Potfolio site showcasing some of my recent work';
 ?>  
