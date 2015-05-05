0287274583b79874517d98cc5480a75d02e108b1
Henry Proudlove <henry.proudlove@gmail.com>
2015-05-05 15:49:22 +0800

    removed extraneous scripts

--- include/global/footer.php
+++ include/global/footer.php
@@ -10,17 +10,8 @@
 			      </feComponentTransfer>
 			  </filter>
 			</svg>
-<!--[if lt IE 9]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo $page->rel_depth() . 'js/vendor/jquery-1.9.0.min.js'; ?>"><\/script>')</script>
-        <script src="<?php //echo $page->rel_depth() . 'js/vendor/froogaloop.min.js';?>"></script>
-		<script src="<?php //echo $page->rel_depth() . 'js/vendor/viewport-units-buggyfill.js';?>"></script>
-        <script src="<?php //echo $page->rel_depth() . 'js/vendor/jquery.cycle2.js';?>"></script>
-        <script src="<?php //echo $page->rel_depth() . 'js/vendor/jquery.cycle2.swipe.min.js';?>"></script>
-        <script src="<?php //echo $page->rel_depth() . 'js/plugins.js';?>"></script>
-        <script src="<?php //echo $page->rel_depth() . 'js/main.js';?>"></script>
         <script src="<?php echo $page->rel_depth() . 'js/scripts.min.js';?>"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
