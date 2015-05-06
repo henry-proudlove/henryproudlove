8f76afc4729db8816c70769c199359a2818be155
Henry Proudlove <henry.proudlove@gmail.com>
2015-05-06 18:01:56 +0800

    Revert "grunt"
    This reverts commit 7de1669d650539f9422ea8d98a7327ed09b2b0f0.

--- include/global/footer.php
+++ include/global/footer.php
@@ -6,6 +6,7 @@
 
         <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>-->
         <script>window.jQuery || document.write('<script src="<?php echo $page->rel_depth() . 'js/vendor/jquery-1.9.0.min.js'; ?>"><\/script>')</script>
+<<<<<<< HEAD
         <script src="<?php echo $page->rel_depth() . 'js/vendor/froogaloop.min.js';?>"></script>
 		<script src="<?php echo $page->rel_depth() . 'js/vendor/viewport-units-buggyfill.js';?>"></script>
         <script src="<?php echo $page->rel_depth() . 'js/vendor/jquery.cycle2.js';?>"></script>
@@ -20,6 +21,16 @@
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>-->
+=======
+        <script src="<?php echo $page->rel_depth() . 'js/scripts.min.js';?>"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            // var _gaq=[['_setAccount','UA-33045219-1'],['_trackPageview']];
+//             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+//             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+//             s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+>>>>>>> parent of 4f7a8e6... grunt
 		
     </body>
 </html>
