4f7a8e678e39090a6138db1e44bfa81d026ba327
Henry Proudlove <henry.proudlove@gmail.com>
2015-05-06 17:25:39 +0800

    grunt

--- include/global/footer.php
+++ include/global/footer.php
@@ -16,7 +16,6 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo $page->rel_depth() . 'js/vendor/jquery-1.9.0.min.js'; ?>"><\/script>')</script>
         <script src="<?php echo $page->rel_depth() . 'js/scripts.min.js';?>"></script>
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
             // var _gaq=[['_setAccount','UA-33045219-1'],['_trackPageview']];
 //             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
